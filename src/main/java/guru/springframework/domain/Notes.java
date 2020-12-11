c41711c8012d08bd5b029b465b45519b029bdf94
kanouelm <ekanoune@yahoo.com>
2020-12-11 11:05:23 +0100

    Assignemnt completed: one-to-one-entity-relationship

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,42 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    private Long id;
+
+    // here we do not need to define the cascade property because we do not
+    // want when a notes is deleted that it recipe is also to be deleted
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob // This is for binary large object field
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
